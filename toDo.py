ca6e00e06095f177b24f1fdbe5127a35beb0ec17
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-18 10:30:22 +0200

    V1

---
+++ toDo.py
@@ -0,0 +1,11 @@
+#Djikstra Algo
+def visitNode():
+    #   1. Node with lowest distance as new starting point
+    #   2. Access its neighbours nodes:
+    #       a. If neighbour node already visited but newdistance > currentdistance : pass
+    #       b. Else : node + edge
+    #   3. Loop
+    pass
+
+while 'inf' in distances:
+    visitNode()
