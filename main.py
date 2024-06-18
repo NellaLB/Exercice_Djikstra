74be886756a7343f774313bd87b0deb45c3c5bf3
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-18 11:09:04 +0200

    V1

--- main.py
+++ main.py
@@ -3,21 +3,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-distances = []
-nbNodes , nbConnections = map(int, input('Nombre de nodes, nombres de connections:   ').split(','))
-print('Les nodes sont notés avec des lettres majuscules.')
-
-connections = np.array([tuple(input('Node1, Node2, Distance:   ').split(',')) for _ in range(nbConnections)])
-print(connections, connections.ndim)
-
 #Djikstra Algo
 def visitNode():
-    #   1. Node with lowest distance as new starting point
+    #   1. Node with lowest distance as starting point:
+    lowestDistance = min(distances)
+    for idDis in range(len(distances)):
+        if distances[idDis] == min(distances) and isCompletelyVisited[idDis] == False:
+            idLowestDistance = idDis
+            print(idLowestDistance)
+            break
     #   2. Access its neighbours nodes:
     #       a. If neighbour node already visited but newdistance > currentdistance : pass
     #       b. Else : node + edge
     #   3. Loop
-    pass
 
-while 'inf' in distances:
+def main():
+    nbNodes , nbConnections = map(int, input('Nombre de nodes, nombres de connections:   ').split(','))
+    print('Les nodes sont notés avec des lettres majuscules.')
+    global distances , isCompletelyVisited, connections
+    distances = [np.inf]*nbNodes
+    isCompletelyVisited = [False]*nbNodes
+    
+    connections = np.array([tuple(input('Node1, Node2, Distance:   ').split(',')) for _ in range(nbConnections)])
+    print(connections, connections.ndim)
+
+    startingNode = input('Starting node (lettre maj.):   ')
+    indexStartingNode = ord(startingNode) - ord('A')
+    distances[indexStartingNode] = 0
+
+    #while np.inf in distances:
     visitNode()
+
+main()
