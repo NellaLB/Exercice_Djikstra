85ec868ebe9a0bc1c7b44df492daefc0f44c621f
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-18 09:32:47 +0200

    V1

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import networkx as nx
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 nbNodes = int(input('Nombre de nodes:  '))
 nbConnections = int(input('Nombre de connections:  '))
 
 distances = ['inf']*nbNodes
-connections = []
+connections = np.array([], ndmin=1)
 for _ in range(nbConnections):
-    connections.append(int(input()))
+    newConnections = np.array(map(int, input('Node en lettre majuscule:   ').split(',')), ndmin=1)
+    np.concatenate((connections, newConnections), axis=0)
+listNodes =[]
 
-graphe.add_nodes_from(nbNodes)
-graphe.add_edges_from(connections)
+graph = nx.Graph()
+graph.add_nodes_from(listNodes)
+graph.add_edges_from(connections)
+
+plt.figure()
+nx.draw(graph)
 plt.show()
 
 startingNode = int(input('Source node:  '))
-distances[startingNode-1] = 0
+distances[ord(startingNode)-ord('A')-1] = 0
 
 #Djikstra Algo
 def visitNode():
-    #   1. Access neighbours nodes:
+    #   1. Access its neighbours nodes:
     #       a. If neighbour node already visited & distance node + neighbour < neighbour : node + neighbour
     #       b. Else set his distance to : edge + node
     #   2. Node with lowest distance as new starting point
+    #   3. Loop
     pass
 
 while 'inf' in distances:
