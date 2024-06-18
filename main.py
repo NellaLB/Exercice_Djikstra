d40196dfc6d45fcbd426ed6dd84399531f0b14b0
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-18 10:27:09 +0200

    V1

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import networkx as nx
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+distances = []
+nbNodes , nbConnections = map(int, input('Nombre de nodes, nombres de connections:   ').split(','))
+print('Les nodes sont notés avec des lettres majuscules.')
 
-nbNodes = int(input('Nombre de nodes:  '))
-nbConnections = int(input('Nombre de connections:  '))
-
-distances = ['inf']*nbNodes
-connections = np.array([], ndmin=1)
-for _ in range(nbConnections):
-    newConnections = np.array(map(int, input('Node en lettre majuscule:   ').split(',')), ndmin=1)
-    np.concatenate((connections, newConnections), axis=0)
-listNodes =[]
-
-graph = nx.Graph()
-graph.add_nodes_from(listNodes)
-graph.add_edges_from(connections)
-
-plt.figure()
-nx.draw(graph)
-plt.show()
-
-startingNode = int(input('Source node:  '))
-distances[ord(startingNode)-ord('A')-1] = 0
+connections = np.array([tuple(input('Node1, Node2, Distance:   ').split(',')) for _ in range(nbConnections)])
+print(connections, connections.ndim)
 
 #Djikstra Algo
 def visitNode():
-    #   1. Access its neighbours nodes:
-    #       a. If neighbour node already visited & distance node + neighbour < neighbour : node + neighbour
-    #       b. Else set his distance to : edge + node
-    #   2. Node with lowest distance as new starting point
+    #   1. Node with lowest distance as new starting point
+    #   2. Access its neighbours nodes:
+    #       a. If neighbour node already visited but newdistance > currentdistance : pass
+    #       b. Else : node + edge
     #   3. Loop
     pass
 
