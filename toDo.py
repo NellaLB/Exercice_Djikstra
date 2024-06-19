f66011e61e308174751fb7bf9f0572f67dadcd7e
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-19 16:10:33 +0200

    V3

--- toDo.py
+++ toDo.py
@@ -1,4 +1,4 @@
 #Djikstra Algo
 #   => One weight per edge check : A,B,2 | B,A,4
 #   => Delete weight for starting point to starting point : B,B,11
-#   => 'Delete' non-accessible node from starting node
+#   => Improve code
